ba76f4f4e52c1babb8163327db0e430375d6579e
kuagabriel8 <kuagabriel8@gmail.com>
2025-08-24 01:01:29 +0800

    lv1 done

--- src/main/java/Chatbot9000.java
+++ src/main/java/Chatbot9000.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Chatbot9000 {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(LINE);
         System.out.println(" Hello! I'm Chatbot9000");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        while (true) {
+            String input = sc.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(LINE);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println(LINE);
+            System.out.println(" " + input);
+            System.out.println(LINE);
+        }
+
+        sc.close();
     }
 }
