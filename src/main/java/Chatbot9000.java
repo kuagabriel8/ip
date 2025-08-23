62fd16e55d8aa4b8338e8d93414042a76d84ec4a
kuagabriel8 <kuagabriel8@gmail.com>
2025-08-24 00:13:10 +0800

    lv0 done

---
+++ src/main/java/Chatbot9000.java
@@ -0,0 +1,11 @@
+public class Chatbot9000 {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Chatbot9000");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
