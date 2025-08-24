cfa76c23a37d0b0029b21a78bf335c76bdfd153b
kuagabriel8 <kuagabriel8@gmail.com>
2025-08-24 11:16:29 +0800

    lv2 done

--- src/main/java/Chatbot9000.java
+++ src/main/java/Chatbot9000.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Chatbot9000 {
     private static final String LINE = "____________________________________________________________";
+    private static final ArrayList<String> inputs = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
 
         System.out.println(LINE);
         System.out.println(" Hello! I'm Chatbot9000");
@@ -19,10 +22,16 @@ public class Chatbot9000 {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println(LINE);
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < inputs.size(); i++) {
+                    System.out.println((i + 1) + ". " + inputs.get(i));
+                }
+            } else {
+                inputs.add(input);
+                System.out.println(LINE);
+                System.out.println("added: " + input);
             }
 
-            System.out.println(LINE);
-            System.out.println(" " + input);
             System.out.println(LINE);
         }
 
